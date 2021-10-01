a9ecd44d942663f3c0d4f93274029b21162ffee0
navyas211@gmail.com <navyas211@gmail.com>
2021-10-01 15:56:03 +0530

    [NAVYA]ADDED: Using Switch Case to Solve Employee Wage

---
+++ src/com/bridgelabz/employeewage/UsingSwitchCase.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.employeewage;
+
+
+public class UsingSwitchCase {   
+	final int FULL_TIME = 2;
+	final int PART_TIME = 1;
+	final int WAGE_PER_HR = 20;
+	int workingHours = 0;
+	int empType;
+	
+	UsingSwitchCase()  {
+		empType = (int) (Math.random() * 100) % 3;
+		
+		switch (empType)
+		{
+		case FULL_TIME:
+			System.out.println("Employee is present Full time ");
+			workingHours = 8;
+			break;
+		case PART_TIME:
+			System.out.println("Employee is present Part time ");
+			workingHours = 4;
+			break;
+		default:
+			System.out.println("Employee is Absent");
+		}
+		int wage = workingHours * WAGE_PER_HR;
+		System.out.println("Employee Daily wage is " + wage);
+	}
+	public static void main(String[] args) {
+		new UsingSwitchCase();
+	
+	}
+
+}
