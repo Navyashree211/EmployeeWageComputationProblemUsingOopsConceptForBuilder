2520421eff7d673e1f4fa0d99570b932aed3a799
navyas211@gmail.com <navyas211@gmail.com>
2021-10-01 13:44:09 +0530

    [NAVYA]ADDED: Welcome to Employee wage Computation Problems

---
+++ src/com/bridgelabz/employeewage/WelcomeEmployees.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewage;
+
+public class WelcomeEmployees {
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage Computation Problem ");
+	}
+
+}
